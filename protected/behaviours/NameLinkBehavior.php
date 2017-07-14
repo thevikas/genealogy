330cdd81fea6c4d24dde376fbfefd7895ae24dc0
Vikas Yadav <vikas@digiscend.com>
2017-07-14 10:33:12 +0530

    Added [link] param namelink

--- protected/behaviours/NameLinkBehavior.php
+++ protected/behaviours/NameLinkBehavior.php
@@ -11,42 +11,48 @@ class NameLinkBehavior extends CActiveRecordBehavior
     var $cacheprefix;
     var $template = "";
     var $callback = false;
-    
+
     private $SALT = 'NameLinkBehavior';
-    
+
     public function attach($owner)
     {
         parent::attach ( $owner );
     }
 
-    public function getnamelink($params = [], $linkprops = [])
+    public function getnamelink($params0 = [], $linkprops = [])
     {
+        $params = array_merge(array_flip($params0),$params0);
+
         list ( $name, $id ) = $this->namelinkprops ();
-        $linkparams = [ 
+        $linkparams = [
                 $this->controller . '/view',
-                'id' => $id 
+                'id' => $id
         ];
         if (! empty ( $params ))
             $linkparams = array_merge ( $linkparams, $params );
-        
+
         $model = $this->getOwner ();
-        
+
         if (empty ( $linkprops ['class'] ))
             $linkprops ['class'] = '';
-        
+
         $linkprops ['class'] .= ' nl ' . get_class ( $model );
-        $linkhtml = CHtml::link ( $name, $linkparams, $linkprops );
+        
+        $linkhtml = $name;
+        if(!isset($params['nolink']))
+            $linkhtml = CHtml::link ( $name, $linkparams, $linkprops );
+        
         if(empty($this->template))
             return $linkhtml;
-        
+
         $mats = [];
         if(!preg_match_all("/{(?<prop>\w+)}/", $this->template,$mats))
             return $linkhtml;
         if(empty($mats['prop']))
             return $linkhtml;
-        
+
         $output = $this->template;
-        
+
         foreach($mats['prop'] as $prop)
         {
             $v = "";
@@ -55,11 +61,11 @@ class NameLinkBehavior extends CActiveRecordBehavior
                 $v = $linkhtml;
             elseif(!empty($this->owner->$propname))
                 $v = $this->owner->$propname;
-            
+
             $output = str_replace("{" . $prop . "}", $v, $output);
         }
         if(!$this->callback) $output = $this->callback($output,$this->owner,$params);
-        
+
         return $output;
     }
 
@@ -68,24 +74,24 @@ class NameLinkBehavior extends CActiveRecordBehavior
         $model = $this->getOwner ();
         $pk = $model->tableSchema->primaryKey;
         if (! empty ( $model->name ))
-            $name = CHtml::encode ( $model->name );        
+            $name = CHtml::encode ( $model->name );
         else
             $name = $model->$pk;
-        
-        return [ 
+
+        return [
                 $name,
-                $model->$pk 
+                $model->$pk
         ];
     }
 
     public function getnameupdatelink()
     {
         list ( $name, $id ) = $this->namelinkprops ();
-        
-        return CHtml::link ( $name, 
-                [ 
+
+        return CHtml::link ( $name,
+                [
                         $this->controller . '/update',
-                        'id' => $id 
+                        'id' => $id
                 ] );
     }
 
@@ -110,21 +116,21 @@ class NameLinkBehavior extends CActiveRecordBehavior
 
     /**
      *
-     * @param CModelEvent $event            
+     * @param CModelEvent $event
      */
     public function afterSave($event)
     {
         $main_owner = $this->getOwner ();
         $ownerPk = $main_owner->getPrimaryKey ();
         $key=$this->getcachedepkey ( $ownerPk );
-        Yii::app ()->setGlobalState ( $key, time () );        
+        Yii::app ()->setGlobalState ( $key, time () );
         //if (isset ( $main_owner->id_project ) && get_class($main_owner) != 'Project')
         //    Yii::app ()->setGlobalState ( $this->getcachedepkey ( $ownerPk,'Project' ), time () );
     }
 
     /**
      * Returns a global state cache dependency object with proper key
-     * 
+     *
      * @param string $depid
      *            PK to be used in generating key, default: owner's PK.
      */
@@ -137,7 +143,7 @@ class NameLinkBehavior extends CActiveRecordBehavior
 
     /**
      * Returns a global state cache dependency object with proper key
-     * 
+     *
      * @param string $depid
      *            PK to be used in generating key, default: owner's PK.
      */
@@ -145,7 +151,7 @@ class NameLinkBehavior extends CActiveRecordBehavior
     {
         if(is_numeric($depid))
             $key=$this->getcachedepkey ( $depid );
-        else 
+        else
             $key = $depid;
         return new CGlobalStateCacheDependency ( $key );
     }
