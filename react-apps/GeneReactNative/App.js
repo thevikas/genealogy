496f3a89a1d595e3c3bef47076f449f454e4abdf
Vikas Yadav <vikas@digiscend.com>
2017-07-21 18:18:38 +0530

    got navigation running

--- react-apps/GeneReactNative/App.js
+++ react-apps/GeneReactNative/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { configureStore } from 'configureStore';
+import { StackNavigator } from 'react-navigation';
+import HomeScreen from 'containers/HomeScreen';
+import ChatScreen from 'containers/ChatScreen';
 const store = configureStore();
+
+
+const SimpleApp = StackNavigator({
+    Home: { screen: HomeScreen },
+    Chat: { screen: ChatScreen }
+});
+
 
 export default class App extends React.Component {
   render() {
     return (
-        <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open 3 up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-  </Provider>
+    <Provider store={store}>
+      <SimpleApp/>
+    </Provider>
     );
   }
 }
---
+++ react-apps/GeneReactNative/src/native/containers/HomeScreen/HomeScreen.js
@@ -0,0 +1,23 @@
+/**
+ * @provi1desModule HomeScreen
+ */
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat')}
+          title="Chat with Lucy"
+        />
+      </View>
+    );
+  }
+}
